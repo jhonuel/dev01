34adf5608121f4502ae491c6c1dbf91d4f5fe5c5
jhonuel <58693667+jhonuel@users.noreply.github.com>
2024-07-29 18:17:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def reload_apache():
     return result.returncode == 0
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
 
